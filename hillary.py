e55a3be6abd5cc7d67087fa7f9c1cd145f719cba
Afukam <97118831+Afukam@users.noreply.github.com>
2023-06-24 15:46:30 +0100

    hillary.py

--- hillary.py
+++ hillary.py
@@ -28,7 +28,19 @@ def mac_change(interface, new_mac):
       subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
       subprocess.call(["ifconfig", interface, "up"])
    except subprocess.CalledProcessError as e:
-      print("[-] Oops, An error occurred while changing the MAC address. Try again, but make sure you have root access before proceeding."
+      print("[-] Oops, An error occurred while changing the MAC address. Try again, but make sure you have root access before proceeding.")
+
+
+def current_mac(interface):
+    try:
+        ifconfig_result = subprocess.check_output(["ifconfig", interface])
+        mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result.decode())
+        if mac_address_search_result:
+            return mac_address_search_result.group(0)
+        else:
+            print(f"[-] Couldn't get a MAC Address from {interface}")
+    except subprocess.CalledProcessError as e:
+        print(f"[-] An error occurred while getting the current MAC address of {interface}.")
 
 options = get_argument()
 current_mac_address = current_mac(options.interface)
